2311ae4f216a56ce56ac54441bd83bc425df3294
Teun van Schagen <551120@student.inholland.nl>
2019-01-07 20:09:39 +0100

    feat(): added the 3 main pages and some layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OverviewComponent } from './overview/overview.component';
+import { CreatorComponent } from './creator/creator.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ModelsComponent } from './models/models.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OverviewComponent,
+    CreatorComponent,
+    ModelsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'creator', component: CreatorComponent},
+      {path: 'overview', component: OverviewComponent},
+      {path: 'models', component: ModelsComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
